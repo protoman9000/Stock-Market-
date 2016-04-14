b313472328b3abc804311620c7665231b22a445c
protoman9000 <oyekan900@yahoo.com>
2016-04-13 22:42:04 -0700

    new update

--- ConsoleApplication25/Program.cs
+++ ConsoleApplication25/Program.cs
@@ -12,6 +12,15 @@ namespace ConsoleApplication25
         {
             string price;
             double x = 0;
+            double ans1 = 0;
+            double ans2 = 0;
+
+            string low;
+            string high;
+
+
+            int j = 0;
+            int count = 0;
             Console.WriteLine("Starting Stok Market Program");
             List<double> stockList = new List<double>();
 
@@ -20,7 +29,28 @@ namespace ConsoleApplication25
             {
                 x = Convert.ToDouble(price);
                 stockList.Add(x);
+                count++;
             }
+
+            for (int k = 0; k < count; k++)
+            {
+                j = k + 1;
+                double result = stockList[k] - stockList[j];
+                if (result > 0 && ans1 == null)
+                {
+                    ans1 = stockList[k];
+                }
+                if (result > 0)
+                {
+                    ans2 = stockList[k];
+                }
+            }
+
+            low = Convert.ToString(ans1);
+            high = Convert.ToString(ans2);
+
+            Console.WriteLine(low + " , " + high);
+
             Console.WriteLine("Ending phase");
 
             Console.ReadKey();
